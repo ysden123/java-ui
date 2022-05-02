424c77eef9553459d446671e8a1f0cd39a74600a
Yuriy Stul <yuriy.stul@whitesourcesoftware.com>
2022-05-02 10:03:27 +0300

    Playing with adding controls

---
+++ Exercises/src/main/java/com/stulsoft/Test2.java
@@ -0,0 +1,38 @@
+package com.stulsoft;
+
+import javax.swing.*;
+import java.awt.*;
+
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
+
+public class Test2 {
+
+    public Test2(){
+        init();
+    }
+
+    public static void main(String[] args) {
+        new Test2();
+    }
+
+    private void init(){
+        JFrame mainFrame = new JFrame("Test1");
+        mainFrame.setSize(400, 400);
+//        mainFrame.setLayout(new GridLayout(2, 2));
+        mainFrame.setLayout(new GridLayout(3, 2));
+
+        JLabel label1 = new JLabel("Label 1", JLabel.CENTER);
+        JLabel label2 = new JLabel("Label 2", JLabel.CENTER);
+        JLabel label3 = new JLabel("Label 3", JLabel.CENTER);
+
+        mainFrame.add(label1);
+        mainFrame.add(label2);
+        mainFrame.add(label3);
+
+        mainFrame.add(new JLabel("Label 4", JLabel.CENTER));
+        mainFrame.add(new JLabel("Label 5", JLabel.CENTER));
+
+        mainFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        mainFrame.setVisible(true);
+    }
+}
